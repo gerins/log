86d4445efad05f32135201073b1251384c626b42
garin_prodigi <gerin.rin@gmail.com>
2024-06-29 13:54:36 +0700

    redesign http trace function

--- http_trace.go
+++ http_trace.go
@@ -3,7 +3,6 @@ package log
 import (
 	"context"
 	"encoding/json"
-	"io"
 	"log/slog"
 	"net/http"
 	"time"
@@ -11,51 +10,47 @@ import (
 
 type (
 	trace struct {
-		statusCode int
-		reqBody    any
-		respHeader any
-		respBody   any
-		timeStart  time.Time
+		method, url        string
+		reqHeader, reqBody any
+		respHeader         http.Header
+		RespBody           []byte
+		statusCode         int
+		timeStart          time.Time
 	}
 )
 
-func NewTrace() trace {
+func NewTrace(method, url string, reqHeader, reqBody any) trace {
 	return trace{
+		method:    method,
+		url:       url,
+		reqHeader: reqHeader,
+		reqBody:   reqBody,
 		timeStart: time.Now(),
 	}
 }
 
-func (t *trace) Save(ctx context.Context, req *http.Request, resp *http.Response) {
-	if req == nil {
-		return
-	}
-
-	// Reading request body
-	if err := json.NewDecoder(req.Body).Decode(&t.reqBody); err != nil {
-		t.reqBody, _ = io.ReadAll(req.Body)
+func (t *trace) Save(ctx context.Context, resp *http.Response) {
+	var respModel any
+	if err := json.Unmarshal(t.RespBody, &respModel); err != nil {
+		respModel = string(t.RespBody)
 	}
 
 	// Reading response body
 	if resp != nil {
 		t.respHeader = resp.Header
 		t.statusCode = resp.StatusCode
-		if err := json.NewDecoder(resp.Body).Decode(&t.respBody); err != nil {
-			if t.respBody, err = io.ReadAll(resp.Body); err != nil {
-				Context(ctx).Errorf("failed reading http response body, %v", err)
-			}
-		}
 	}
 
 	globalLogger.LogAttrs(ctx, LevelTrace, "",
 		slog.String("caller", GetCaller("", 3)),
 		slog.String(processID, Context(ctx).ProcessID()),
-		slog.String("method", req.Method),
-		slog.String("url", req.URL.String()),
+		slog.String("method", t.method),
+		slog.String("url", t.url),
 		slog.Int("statusCode", t.statusCode),
 		slog.Int64("requestDuration", time.Since(t.timeStart).Milliseconds()),
-		slog.Any("requestHeader", req.Header),
+		slog.Any("requestHeader", t.reqHeader),
 		slog.Any("requestBody", t.reqBody),
 		slog.Any("responseHeader", t.respHeader),
-		slog.Any("responseBody", t.respBody),
+		slog.Any("responseBody", respModel),
 	)
 }
